040e2128722e94e2189d41934e7342b089826f02
amaralc <amaral.calil@gmail.com>
2020-08-12 09:14:36 -0300

    FIX / Adiciona comentários e sugere edição de nome de variável

--- src/components/P5Canvas/index.js
+++ src/components/P5Canvas/index.js
@@ -18,21 +18,29 @@ export default class P5Canvas extends React.Component {
       s.createCanvas(400, 400);
     };
 
+    /** X coordinate of the laser source */
     const laserSourcePositionX = 0;
+
+    /** Y coordinate of the laser source */
     const laserSourcePositionY = 0;
+
+    /** Diameter of the laser source */
     const laserSourceDiameter = 15;
 
     /** The red scale begins in 0 (white) and end in 255(red) */
     const laserSourceRedFill = 255;
 
     s.draw = () => {
+      /** Draw gray background with R, G and B with same value */
       s.background(200);
 
-      /** Laser Source hasn't a stroke */
+      /** Laser Source has no stroke (border line) */
       s.noStroke();
+
       /** Red fill color */
       s.fill(laserSourceRedFill, 0, 0);
-      /** It's a 15px diameter circle, with the center positioned in coordinates (0,0) */
+
+      /** Create 15px diameter circle, with the center positioned in coordinates (0,0) */
       s.ellipse(
         laserSourcePositionX,
         laserSourcePositionY,
@@ -40,24 +48,26 @@ export default class P5Canvas extends React.Component {
         laserSourceDiameter
       );
 
-      /** The variable "laserStartPosition" refers to the position vector of the Laser beam begin */
-      const laserStartPosition = s.createVector(0, 0);
+      /** The variable "laserRayStartPosition" refers to the position vector of the Laser beam begin */
+      const laserRayStartPosition = s.createVector(0, 0);
 
-      /** The variable "laserEndPosition" refers to the mouse position vector */
-      const laserEndPosition = s.createVector(s.mouseX, s.mouseY);
+      /** The variable "laserRayEndPosition" refers to the mouse position vector */
+      const laserRayEndPosition = s.createVector(s.mouseX, s.mouseY);
 
       /**
        * The vector "laserVector" is the result of the subtraction between the mouse vector and the pos vector;
        * This relation is found from the concept of vector sum;
        * The method sub takes the vectors that will be subtracted as parameters;
        */
-      const laserVector = P5.Vector.sub(laserEndPosition, laserStartPosition);
+      const laserVector = P5.Vector.sub(
+        laserRayEndPosition,
+        laserRayStartPosition
+      );
 
       /**
        * If mouse is pressed, a line, that represents the laser beam is shot from position (0,0)
        * until position where it was clicked
        */
-
       if (s.mouseIsPressed) {
         s.stroke(255, 0, 0);
         s.strokeWeight(2);
