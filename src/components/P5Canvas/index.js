f71a25128ec5ffcd44bd10a37599eeafdda8440f
Artur Della Favera <arturdellafavera@gmail.com>
2020-08-12 08:55:55 -0300

    CODE / Troca parâmetros por variáveis que facilitam o entendimento

--- src/components/P5Canvas/index.js
+++ src/components/P5Canvas/index.js
@@ -18,28 +18,40 @@ export default class P5Canvas extends React.Component {
       s.createCanvas(400, 400);
     };
 
+    const laserSourcePositionX = 0;
+    const laserSourcePositionY = 0;
+    const laserSourceDiameter = 15;
+
+    /** The red scale begins in 0 (white) and end in 255(red) */
+    const laserSourceRedFill = 255;
+
     s.draw = () => {
       s.background(200);
 
       /** Laser Source hasn't a stroke */
       s.noStroke();
       /** Red fill color */
-      s.fill(255, 0, 0);
+      s.fill(laserSourceRedFill, 0, 0);
       /** It's a 15px diameter circle, with the center positioned in coordinates (0,0) */
-      s.ellipse(0, 0, 15, 15);
+      s.ellipse(
+        laserSourcePositionX,
+        laserSourcePositionY,
+        laserSourceDiameter,
+        laserSourceDiameter
+      );
 
-      /** The variable "pos" refers to the position vector of the Laser beam begin */
-      const pos = s.createVector(0, 0);
+      /** The variable "laserStartPosition" refers to the position vector of the Laser beam begin */
+      const laserStartPosition = s.createVector(0, 0);
 
-      /** The variable "mouse" refers to the mouse position vector */
-      const mouse = s.createVector(s.mouseX, s.mouseY);
+      /** The variable "laserEndPosition" refers to the mouse position vector */
+      const laserEndPosition = s.createVector(s.mouseX, s.mouseY);
 
       /**
-       * The vector "v" is the result of the subtraction between the mouse vector and the pos vector;
+       * The vector "laserVector" is the result of the subtraction between the mouse vector and the pos vector;
        * This relation is found from the concept of vector sum;
        * The method sub takes the vectors that will be subtracted as parameters;
        */
-      const v = P5.Vector.sub(mouse, pos);
+      const laserVector = P5.Vector.sub(laserEndPosition, laserStartPosition);
 
       /**
        * If mouse is pressed, a line, that represents the laser beam is shot from position (0,0)
@@ -49,7 +61,7 @@ export default class P5Canvas extends React.Component {
       if (s.mouseIsPressed) {
         s.stroke(255, 0, 0);
         s.strokeWeight(2);
-        s.line(0, 0, v.x, v.y);
+        s.line(0, 0, laserVector.x, laserVector.y);
       }
     };
   };
