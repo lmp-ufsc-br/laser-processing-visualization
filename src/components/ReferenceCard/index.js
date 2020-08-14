62d201ed65de6ec85860d1d22adc4c1ec3ca3365
amaralc <amaral.calil@gmail.com>
2020-08-14 12:58:39 -0300

    FIX / Open reference in new tab

--- src/components/ReferenceCard/index.js
+++ src/components/ReferenceCard/index.js
@@ -4,7 +4,7 @@ import { Card, TextContent } from './styles';
 
 export default function ReferenceCard(props) {
   return (
-    <Card href={props.route}>
+    <Card href={props.route} target="_blank">
       <img src={props.imgSrc} alt="" />
       <TextContent>
         <h2>{props.title}</h2>
