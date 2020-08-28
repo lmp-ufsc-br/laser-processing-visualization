54e2bc31a5331e995f4fdeb719fc073509f42339
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-08-28 13:18:24 -0300

    FEATURE / Adiciona espaçamento superior

--- src/pages/Home/TitleCard.js
+++ src/pages/Home/TitleCard.js
@@ -16,6 +16,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
+    marginTop: theme.spacing(3),
   },
   overlay: {
     position: 'absolute',
