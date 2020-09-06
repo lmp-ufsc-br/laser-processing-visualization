27324f4c9c2f349b3086d7e1831a6c3ffba6ea4d
amaralc <amaral.calil@gmail.com>
2020-09-06 17:32:52 -0300

    FIX / Relocate files and fix style

--- src/pages/Blog/routes.js
+++ src/pages/Blog/routes.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Route from '../../routes/Route';
 
 import Home from '../Home';
-import AABlogTemplate from './BlogPosts/AABlogTemplate';
+import Template from './BlogPosts/Template';
 import LaserMatter from './BlogPosts/LaserMatter';
-import Resistance from './BlogPosts/Resistance';
+import Resistance from './BlogPosts/Resistance2';
 
 export default function Routes() {
   return (
     <>
       <Route exact path="/blog" component={Home} />
-      <Route path="/template-post" component={AABlogTemplate} />
+      <Route path="/template-post" component={Template} />
       <Route path="/laser-matter" component={LaserMatter} />
       <Route path="/resistance" component={Resistance} />
     </>
