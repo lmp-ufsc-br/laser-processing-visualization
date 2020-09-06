4968326b53f9c431e1cb8158bba4271ba74e1ce0
amaralc <amaral.calil@gmail.com>
2020-09-06 11:24:49 -0300

    FIX / Use component to export md

--- src/pages/Blog/BlogPosts/AABlogTemplate/content.js
+++ src/pages/Blog/BlogPosts/AABlogTemplate/content.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Markdown from 'markdown-to-jsx';
 import articleMd from './content.md';
+import article from './contentExport';
 
 class Article extends Component {
   constructor(props) {
@@ -19,7 +20,7 @@ class Article extends Component {
   render() {
     return (
       <div className="article">
-        <Markdown>{this.state.md}</Markdown>
+        <Markdown>{article}</Markdown>
       </div>
     );
   }
---
+++ src/pages/Blog/BlogPosts/AABlogTemplate/contentExport.js
@@ -0,0 +1,31 @@
+import myImage from './laser.png';
+
+export default `
+
+##### Simulação Laser
+
+Este é um teste.
+
+
+Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis
+ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id
+elit.
+
+<img alt="test" title="my img" src="https://miro.medium.com/max/1400/0*lzRmzAy5OICef7rK.png"/>
+
+![my image](${myImage})
+
+
+Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur
+purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.
+
+
+##### Referências
+
+<a href="/">
+  <p>Continue lendo...</p>
+</a>
+
+[continue lendo novamente](/)
+
+`;
