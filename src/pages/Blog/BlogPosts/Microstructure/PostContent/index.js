713ebf7d094560c10cfa1b060f8b879e72c3b066
amaralc <amaral.calil@gmail.com>
2020-09-14 08:57:22 -0300

    FEATURE / Add article microstructure

---
+++ src/pages/Blog/BlogPosts/Microstructure/PostContent/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ContentWrapper } from './styles';
+import Content from './Article';
+
+function BlogContent(props) {
+  return (
+    <ContentWrapper>
+      <h6>{props.title}</h6>
+      <hr />
+
+      <Content />
+    </ContentWrapper>
+  );
+}
+
+export default BlogContent;
