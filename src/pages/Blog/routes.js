e276d373b1e34ece3580cedf3dbd04336626aef6
Artur Della Favera <arturdellafavera@gmail.com>
2020-10-21 19:10:34 -0300

    FEATURE / Cria rota para segunda abordagem de LaserMatter

--- src/pages/Blog/routes.js
+++ src/pages/Blog/routes.js
@@ -4,6 +4,7 @@ import Route from '../../routes/Route';
 import Home from '../Home';
 import Template from './BlogPosts/Template';
 import LaserMatter from './BlogPosts/LaserMatter';
+import LaserMatter2 from './BlogPosts/LaserMatter-SecondApproach';
 import Resistance from './BlogPosts/Resistance';
 import LaserRay from './BlogPosts/LaserRay';
 import Microstructure from './BlogPosts/Microstructure';
@@ -14,6 +15,7 @@ export default function Routes() {
       <Route exact path="/blog" component={Home} />
       <Route path="/template-post" component={Template} />
       <Route path="/laser-matter" component={LaserMatter} />
+      <Route path="/laser-matter2" component={LaserMatter2} />
       <Route path="/resistance" component={Resistance} />
       <Route path="/laser-ray" component={LaserRay} />
       <Route path="/microstructure" component={Microstructure} />
