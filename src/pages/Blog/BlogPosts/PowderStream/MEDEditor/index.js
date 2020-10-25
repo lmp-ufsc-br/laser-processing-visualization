9192598eba63ff86737224f3efbe8e52d27b7c3a
amaralc <amaral.calil@gmail.com>
2020-10-25 15:59:37 -0300

    FEATURE / Add katex

--- src/pages/Blog/BlogPosts/PowderStream/MEDEditor/index.js
+++ src/pages/Blog/BlogPosts/PowderStream/MEDEditor/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import MDEditor from '@uiw/react-md-editor';
+import katex from 'katex';
+import 'katex/dist/katex.css';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -9,13 +11,46 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+const renderers = {
+  inlineCode: ({ children }) => {
+    if (/^\$\$(.*)\$\$/.test(children)) {
+      const html = katex.renderToString(
+        children.replace(/^\$\$(.*)\$\$/, '$1'),
+        {
+          throwOnError: false,
+        }
+      );
+      return <code dangerouslySetInnerHTML={{ __html: html }} />;
+    }
+    return children;
+  },
+  code: ({ children, language, value }) => {
+    if (language.toLocaleLowerCase() === 'katex') {
+      const html = katex.renderToString(value, {
+        throwOnError: false,
+      });
+      return (
+        <pre>
+          <code dangerouslySetInnerHTML={{ __html: html }} />
+        </pre>
+      );
+    }
+    return children;
+  },
+};
+
 export default function App() {
   const classes = useStyles();
 
   const [value, setValue] = React.useState('**Hello world!!!**');
   return (
     <div className={classes.container}>
-      <MDEditor value={value} onChange={setValue} />
+      <MDEditor
+        value={value}
+        onChange={setValue}
+        preview="edit"
+        previewOptions={{ renderers }}
+      />
       <MDEditor.Markdown source={value} />
     </div>
   );
